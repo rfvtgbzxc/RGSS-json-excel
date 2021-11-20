36e33ede0aeea92846775b8f1d5ccc1f92886bf1
Xu <Chen>
2021-11-20 14:38:09 +0800

    2021.11.20
    it's ok!

---
+++ CodeAndExample/json_excel.js
@@ -0,0 +1,146 @@
+const fs = require('fs');
+const ExcelJS = require('exceljs');
+const { bindFiles, words } = require('./config');
+const { nextTick } = require('process');
+function readFileUsePromise(filename) {
+  return new Promise((resolve) => {
+    fs.readFile(`./${filename}`, "utf8", (err, result) => {
+      resolve(result)
+    });
+  });
+}
+function writeFileUsePromise(filename, data) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(filename, data, (err) => {
+      if (err) {
+        console.log(err);
+        resolve(false);
+      }
+      else {
+        resolve(true);
+      }
+    })
+  });
+}
+function getFileExistenceUsePromise(filename) {
+  return new Promise((resolve, reject) => {
+    fs.stat(filename, (err, stats) => {
+      if (err) {
+        resolve(false);
+      }
+      else {
+        resolve(true);
+      }
+    })
+  });
+}
+function getRowIndex2Id(sheet) {
+  // 获取基于id的rowIndex索引表
+  const lastRow = sheet.lastRow;
+  const rowIndex2Id = {};
+  for (let rowIndex = 2; rowIndex <= lastRow.number; rowIndex++) {
+    rowIndex2Id[rowIndex] = sheet.getCell(rowIndex, 1).value;
+  }
+  return rowIndex2Id;
+}
+async function JsonToExcel() {
+  const workbook = new ExcelJS.Workbook();
+  if (await getFileExistenceUsePromise("DataBase.xlsx")) {
+    await workbook.xlsx.readFile("DataBase.xlsx");
+  }
+  for (let pageName in bindFiles) {
+    const data = await readFileUsePromise(`Json/${pageName}.json`);
+    const lines = JSON.parse(data);
+    let sheet = workbook.getWorksheet(pageName);
+    let rowIndex2Id = null;
+    if (!!!sheet) {
+      sheet = workbook.addWorksheet(pageName);
+    }
+    else {
+      rowIndex2Id = getRowIndex2Id(sheet);
+    }
+    sheet.views = [
+      { state: 'frozen', ySplit: 1 }
+    ];
+    // 第一行是属性名称
+    let colIndex = 1;
+    let columns;
+    if (bindFiles[pageName] === "AllAvailable") {
+      columns = Object.keys(words[pageName]);
+    }
+    else {
+      columns = bindFiles[pageName];
+    }
+    for (let attrName of columns) {
+      sheet.getColumn(colIndex).header = words[pageName][attrName];
+      colIndex++;
+    }
+    // 写属性值
+    for (let rowIndex = 2; rowIndex < lines.length; rowIndex++) {
+      for (let i = 0; i < columns.length; i++) {
+        const requestId = rowIndex2Id === null ? rowIndex - 1 : rowIndex2Id[rowIndex];
+        sheet.getRow(rowIndex).getCell(1 + i).value = lines[requestId][`:@${columns[i]}`];
+      }
+    }
+  }
+  await workbook.xlsx.writeFile("DataBase.xlsx");
+  console.log("JsonToExcel run successfully !")
+}
+async function ExcelToJson() {
+  for (let pageName in bindFiles) {
+    if (!await getFileExistenceUsePromise(`Json/${pageName}.json`)) {
+      console.log(`ExcelToJson error: need file ${pageName}.json!`);
+      return;
+    }
+  }
+  const workbook = new ExcelJS.Workbook();
+  if (await getFileExistenceUsePromise("DataBase.xlsx")) {
+    await workbook.xlsx.readFile("DataBase.xlsx");
+  }
+  else {
+    console.log(`ExcelToJson error: need file DataBase.xlsx!`);
+    return;
+  }
+  for (let pageName in bindFiles) {
+    const sheet = workbook.getWorksheet(pageName);
+    if (!!!sheet) {
+      continue;
+    }
+    const jsonData = await readFileUsePromise(`Json/${pageName}.json`);
+    const lines = JSON.parse(jsonData);
+    const rowIndex2Id = getRowIndex2Id(sheet);
+    let columns;
+    if (bindFiles[pageName] === "AllAvailable") {
+      columns = Object.keys(words[pageName]);
+    }
+    else {
+      columns = bindFiles[pageName];
+    }
+    // 写属性值
+    for (let rowIndex = 2; rowIndex < lines.length; rowIndex++) {
+      for (let i = 0; i < columns.length; i++) {
+        const requestId = rowIndex2Id === null ? rowIndex - 1 : rowIndex2Id[rowIndex];
+        lines[requestId][`:@${columns[i]}`] = sheet.getCell(rowIndex, 1 + i).value;
+      }
+    }
+    if (await writeFileUsePromise(`Json/${pageName}.json`, JSON.stringify(lines))) {
+      console.log(`Write file ${pageName}.json successed!`);
+    }
+    else {
+      console.log(`Write file ${pageName}.json failed.`);
+    }
+  }
+  console.log("ExcelToJson run successfully !")
+}
+//JsonToExcel();
+//ExcelToJson();
+switch (process.argv[2]) {
+  case "2excel":
+    JsonToExcel();
+    break;
+  case "2json":
+    ExcelToJson();
+    break;
+  default:
+    console.log(`unrecognized param ${process.argv[2]}, choose '2excel' or '2json'.`)
+}
--- README-ch.md
+++ README-ch.md
@@ -4,22 +4,67 @@
 ### 该项目的组件相当轻量，部分内容是为RGSS定制的，如果要在非RGSS环境下运行，需要自行调整。
 
 # 组件（√代表已完成）
-* rgss-serializer
+* rgss-serializer √
   * 用于将原生对象转换为json，参考了Marshal的数据结构。
   * 其中为RGSS定制的部分是对Table类型的处理。
-* json-to-excel
+* json-to-excel √
   * 用于将json转换为excel
-* excel-to-rxData
+* excel-to-rxData √
   * 用于将excel转换为原生对象
+* more-smart-rxData_json_excel
+  * 不再需要nodejs和json作为中间态的脚本
+
+# 初始化（安装库和依赖）
+### 1.安装nodejs
+### 2.在CodeAndExample目录下运行指令，安装js库：
+``` shell
+npm install
+```
+### 3.安装ruby
+### 4.运行指令，安装ruby库
+``` shell
+gem install read_excel
+```
 
 # 理论工作流程
-### 1. ：
-```ruby
-file1 = File.open("Data/Actors.rxdata","rb")
-actors = Marshal.load(file1)
-file2 = File.open("Actors.json", "w")
-file2.write(Serializer.new.stringify(actors))
-file1.close
-file2.close
+## 一、数据的导出
+### 1. 将Data文件夹放入CodeAndExample文件夹
+### 2. 运行指令：
+``` shell
+ruby rxdata_json_excel.rb 2json
 ```
-### 2. 还没想好，作者不太了解excel里如何使用引用，因此在json到excel互转的过程中可能会只允许导入一部分非引用类型的数据（这是数据里的绝大部分）。
+期待结果：
+```
+Write file Actors.json successed!
+Write file Enemies.json successed!
+Write file Weapons.json successed!
+All successed!
+```
+### 3.运行指令
+```
+node json-excel 2excel
+```
+期待结果：
+```
+JsonToExcel run successfully !
+```
+### 4.此时CodeAndExample目录下出现文件"Database.xlsx"，开始使用吧！
+
+## 二、数据的导入
+### 运行指令：
+```
+ruby rxdata_json_excel.rb 2rxdata
+```
+期待结果：
+```
+Write file Actors.rxdata successed!
+Write file Enemies.rxdata successed!
+Write file Weapons.rxdata successed!
+All successed!
+```
+因为这个过程对xlsx文件是只读的，所以可以不用关闭excel，保存后就可以运行指令对数据进行同步。
+
+## 三、填写配置文件
+### Demo中仅提供了Actors，Weapons和Enemies的数据，要处理更多的文件和数据，需要填写config.rb和config.js。
+- bind_files：描述要处理哪些数据库成员，对应值可以是"AllAvailable"或者一个数组，只有数组里的成员的数据会被同步。
+- words：用于在表格中，调整数据的名称。
